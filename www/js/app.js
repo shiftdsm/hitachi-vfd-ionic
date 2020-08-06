45128f7434864b2eea9f94a1cc1461e798c13255
Clayton Brady <cjbrady09@gmail.com>
2020-08-06 10:31:37 -0500

    updates witth capacitor

--- www/js/app.js
+++ www/js/app.js
@@ -12,7 +12,8 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
     // for form inputs)
     if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(false);
+      window.Keyboard.hideFormAccessoryBar(false);
       cordova.plugins.Keyboard.disableScroll(true);
         
     }
